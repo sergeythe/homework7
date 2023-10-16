249977c7cd73ca2675145dbd6d2ee1bba213de59
sergeythe <sergey2902g@gmail.com>
2023-10-16 13:26:58 +0300

    commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<style scoped>
+.hello {
+  width: 40px;
+  height: 40px;
+  margin: auto;
+  background-color: red;
+}
+</style>
